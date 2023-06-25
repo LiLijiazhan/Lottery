50964dd0ab2df6f1095e8fdefdf4d899e484920e
lijiazhan <lijiazhan@hyperchain.cn>
2023-06-25 18:14:25 +0800

    substractionStock

--- lottery-infrastructure/src/main/java/cn/itedus/lottery/infrastructure/dao/IStrategyDao.java
+++ lottery-infrastructure/src/main/java/cn/itedus/lottery/infrastructure/dao/IStrategyDao.java
@@ -1,7 +1,10 @@
 package cn.itedus.lottery.infrastructure.dao;
 
 import cn.itedus.lottery.infrastructure.po.Strategy;
+import cn.itedus.lottery.infrastructure.po.StrategyDetail;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
@@ -10,7 +13,22 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface IStrategyDao {
-
+    /**
+     * 查询策略表详细配置
+     * @param strategyId 策略ID
+     * @return           返回结果
+     */
     Strategy queryStrategy(Long strategyId);
-
+    /**
+     * 查询无库存策略奖品ID
+     * @param strategyId 策略ID
+     * @return           返回结果
+     */
+    List<String> queryNoStockStrategyAwardList(Long strategyId);
+    /**
+     * 扣减库存
+     * @param strategyDetailReq 策略ID、奖品ID
+     * @return                  返回结果
+     */
+    int deductStock(StrategyDetail strategyDetailReq);
 }
