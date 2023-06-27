02959d79c8b99716006b1acc1de098e6ddc954e5
lijiazhan <lijiazhan@hyperchain.cn>
2023-06-27 10:58:39 +0800

    feat idgenerator

--- lottery-common/src/main/java/cn/itedus/lottery/common/Constants.java
+++ lottery-common/src/main/java/cn/itedus/lottery/common/Constants.java
@@ -257,4 +257,12 @@ public class Constants {
         }
     }
 
+    public enum Ids{
+        //雪花算法
+        SnowFlake,
+        //日期算法
+        ShortCode,
+        //随机算法
+        RandomNumeric
+    }
 }
